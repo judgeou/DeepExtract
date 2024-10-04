116f5cd264ff0db233f11d751d5906b7adba05a6
judgeou <judgeou@vip.qq.com>
2024-10-04 18:20:32 +0800

    add icon

--- DeepExtract/Form1.Designer.cs
+++ DeepExtract/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
@@ -220,6 +221,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "DeepExtract";
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
